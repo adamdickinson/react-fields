8d29ecbbda7141e3f889412a9ccc82b93a7d7a05
Adam Dickinson <adam@renegadedigital.com.au>
2018-02-27 15:15:09 +1100

    Export Hotfix
    - Fix path bug
    - Fix main bug (package.json)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import FieldContainer from "./component/FieldContainer"
+import FieldContainer from "./components/FieldContainer"
 import { validate, validators } from "./helpers/validate"
 module.exports = { FieldContainer, validate, validators }
